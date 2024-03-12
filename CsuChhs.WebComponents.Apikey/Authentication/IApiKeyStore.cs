613d60521b9d09a19116365c47f8defeea096eb0
Derek Stegelman <dstegelman@gmail.com>
2024-03-12 14:25:47 -0600

    Change namespace and add new components

---
+++ CsuChhs.WebComponents.Apikey/Authentication/IApiKeyStore.cs
@@ -0,0 +1,12 @@
+﻿namespace CsuChhs.WebComponents.Apikey.Authentication
+{
+    public interface IApiKeyStore
+    {
+        string GetKey();
+
+        string GetWriteKey()
+        {
+            return string.Empty;
+        }
+    }
+}
